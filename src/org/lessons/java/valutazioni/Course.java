b3249cb42c6fd709b4199b36dd1c09b15a408161
xgiannix <gianni.demare@hotmail.it>
2023-12-13 15:18:07 +0100

    First Commit

--- src/org/lessons/java/valutazioni/Course.java
+++ src/org/lessons/java/valutazioni/Course.java
@@ -1,2 +1,52 @@
-package org.lessons.java.valutazioni;public class Course {
+package org.lessons.java.valutazioni;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class Course extends Student{
+    // ATTRIBUTI
+    private ArrayList<Student> listOfStudents;
+
+    // COSTRUTTORI
+    public Course(){
+
+    }
+
+    public Course(int id, int percentageOfAbsences, BigDecimal averageGradePoints, ArrayList<Student> listOfStudents) {
+        super(id, percentageOfAbsences, averageGradePoints);
+        this.listOfStudents = listOfStudents;
+    }
+
+    // GETTER E SETTER
+
+    public ArrayList<Student> getListOfStudents() {
+        return listOfStudents;
+    }
+
+    public void setListOfStudents(ArrayList<Student> listOfStudents) {
+        this.listOfStudents = listOfStudents;
+    }
+
+
+    // METODI
+
+    // METODO PER MOSTRARE A VIDEO L'ELENCO DI TUTTI GLI STUDENTE CON I RELATIVI DATI
+    public void showStudentList(){
+        System.out.println("List of students in the course: ");
+        for (Student student : listOfStudents) {
+            System.out.println(" Id: " + student.getId() + " Absences: " + student.getPercentageOfAbsences() + " Average Grade Points: " + student.getAverageGradePoints());
+        }
+    }
+
+    // METODO PER AGGIUNGERE UNO STUDENTE ALLA LISTA
+    public void addStudent(Student studente) {
+        listOfStudents.add(studente);
+        System.out.println("Student added to the course!");
+    }
+
+    // METODO PER RIMUOVERE UNO STUDENTE DALLA LISTA
+    public void removeStudent(){
+
+    }
+
 }
--- src/org/lessons/java/valutazioni/Student.java
+++ src/org/lessons/java/valutazioni/Student.java
@@ -1,2 +1,67 @@
-package org.lessons.java.valutazioni;public class Student {
+package org.lessons.java.valutazioni;
+
+import java.math.BigDecimal;
+
+public class Student{
+    // ATTRIBUTI
+    private int id;
+    private int percentageOfAbsences;
+    private BigDecimal averageGradePoints;
+
+    // COSTRUTTORI
+    public Student(){
+
+    }
+
+    public Student(int id, int percentageOfAbsences, BigDecimal averageGradePoints) {
+        this.id = id;
+        this.percentageOfAbsences = percentageOfAbsences;
+        this.averageGradePoints = averageGradePoints;
+    }
+
+    // GETTER E SETTER
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPercentageOfAbsences() {
+        return percentageOfAbsences;
+    }
+
+    public void setPercentageOfAbsences(int percentageOfAbsences) {
+        this.percentageOfAbsences = percentageOfAbsences;
+    }
+
+    public BigDecimal getAverageGradePoints() {
+        return averageGradePoints;
+    }
+
+    public void setAverageGradePoints(BigDecimal averageGradePoints) {
+        this.averageGradePoints = averageGradePoints;
+    }
+
+
+    // METODI
+
+    // METODO PER CALCOLARE L'ESITO DELLO STUDENTE
+    public void calculateOutcome() {
+        BigDecimal mediumThresholdPromotion = new BigDecimal("2.0");
+        if (percentageOfAbsences > 50) {
+            System.out.println("Studente " + getId() + ": Bocciato (troppe assenze)");
+        } else if (percentageOfAbsences >= 25 && percentageOfAbsences <= 50 && averageGradePoints.compareTo(mediumThresholdPromotion) > 0) {
+            System.out.println("Studente " + getId() + ": Promosso");
+        } else if (percentageOfAbsences < 25 && averageGradePoints.compareTo(mediumThresholdPromotion) >= 0) {
+            System.out.println("Studente " + getId() + ": Promosso");
+        } else {
+            System.out.println("Studente " + getId() + ": Bocciato");
+        }
+    }
 }
+
+
+
